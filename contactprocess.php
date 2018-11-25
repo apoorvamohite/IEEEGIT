ce79d7318c716601c8982426b7dd10ab7c022537
apoorvamohite <33003637+apoorvamohite@users.noreply.github.com>
2018-11-25 18:52:37 +0530

    Add files via upload

--- contactprocess.php
+++ contactprocess.php
@@ -5,8 +5,9 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $msg = $_POST['message'];
+$date = date("Y-m-d");
 
-$sql = "INSERT INTO contact(name, email, subject, msg) VALUES ('$name', '$email', '$subject', '$msg')";
+$sql = "INSERT INTO contact(msgDate, name, email, subject, msg) VALUES ('$date','$name', '$email', '$subject', '$msg')";
 mysqli_query($con, $sql);
 header("Location:index.php?msgsent=true#sendmessage");
 ?>
