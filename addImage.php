2ce2a4cc9be0a85968d57440b57d30476c1e5d59
mrunalambekar <45233550+mrunalambekar@users.noreply.github.com>
2018-11-25 23:33:43 +0530

    Update addImage.php

--- addImage.php
+++ addImage.php
@@ -48,16 +48,6 @@
                </button>               
                 <a class="navbar-brand" href="#">All Events</a>
                </div>
-                <div class="collapse navbar-collapse">                    
-                    <ul class="nav navbar-nav navbar-right">                        
-                        <li>
-                            <a href="logout.php">
-                                <p>Log out</p>
-                            </a>
-                        </li>
-						<li class="separator hidden-lg hidden-md"></li>
-                    </ul>
-                </div>
             </div>
         </nav>
 
